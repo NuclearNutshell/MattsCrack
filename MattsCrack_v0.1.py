da2269477a7ad692834ae3b595850acfe86ac1bd
NuclearNutshell <matthewk_ames@hotmail.com>
2024-11-04 22:56:00 +0000

    Changes Made: -   Added Capitalisation -   Added deduplication -   Changed output from console log to text file

--- MattsCrack_v0.1.py
+++ MattsCrack_v0.1.py
@@ -1,4 +1,4 @@
-
+import itertools
 
 
 ##################################
@@ -23,15 +23,10 @@ def create_org_wordlist(name, dob):
   for part in name_parts:
     wordlist.append(part)
 
-  # Loop through the date of birth parts and add them to the wordlist
-  for part in dob_parts:
-    wordlist.append(part)
-
   # Loop through the name parts and the date of birth parts and combine them in different ways
   for i in range(len(name_parts)):
     for j in range(len(dob_parts)):
       # Add the name part followed by the date of birth part, db, yob and yob_full
-      wordlist.append(name_parts[i] + dob_parts[j])
       wordlist.append(name_parts[i] + yob)
       wordlist.append(name_parts[i] + yob_full)
       wordlist.append(name_parts[i] + db)
@@ -81,21 +76,17 @@ def create_person_wordlist(name, dob):
 
   # Loop through the name parts and the date of birth parts and combine them in different ways
   for i in range(len(name_parts)):
-    for j in range(len(dob_parts)):
-      # Add the name part followed by the date of birth part, db, yob and yob_full
-      wordlist.append(name_parts[i] + dob_parts[j])
-      wordlist.append(name_parts[i] + yob)
-      wordlist.append(name_parts[i] + yob_full)
-      wordlist.append(name_parts[i] + db)
+    # Add the name part followed by the date of birth part, db, yob and yob_full
+    wordlist.append(name_parts[i] + yob)
+    wordlist.append(name_parts[i] + yob_full)
+    wordlist.append(name_parts[i] + db)
 
   # Loop through the Nickname parts and the date of birth parts and combine them in different ways
   for i in range(len(nick_parts)):
-    for j in range(len(dob_parts)):
-      # Add the name part followed by the date of birth part, db, yob and yob_full
-      wordlist.append(nick_parts[i] + dob_parts[j])
-      wordlist.append(nick_parts[i] + yob)
-      wordlist.append(nick_parts[i] + yob_full)
-      wordlist.append(nick_parts[i] + db)
+    # Add the name part followed by the date of birth part, db, yob and yob_full
+    wordlist.append(nick_parts[i] + yob)
+    wordlist.append(nick_parts[i] + yob_full)
+    wordlist.append(nick_parts[i] + db)
 
   # Loop through pet name parts and put the dob at the end
   for l in range(len(petNames_parts)):
@@ -111,21 +102,17 @@ def create_person_wordlist(name, dob):
 
   # Loop through the name parts and the date of birth parts and combine them in different ways
   for i in range(len(name_parts)):
-    for j in range(len(dob_parts)):
-      # Add the name part followed by the date of birth part, db, yob and yob_full
-      wordlist.append(dob_parts[j] + name_parts[i])
-      wordlist.append(yob + name_parts[i])
-      wordlist.append(yob_full + name_parts[i])
-      wordlist.append(db + name_parts[i])
+    # Add the name part followed by the date of birth part, db, yob and yob_full
+    wordlist.append(yob + name_parts[i])
+    wordlist.append(yob_full + name_parts[i])
+    wordlist.append(db + name_parts[i])
 
   # Loop through the Nickname parts and the date of birth parts and combine them in different ways
   for i in range(len(nick_parts)):
-    for j in range(len(dob_parts)):
-      # Add the name part followed by the date of birth part, db, yob and yob_full
-      wordlist.append(dob_parts[j] + nick_parts[i])
-      wordlist.append(yob + nick_parts[i])
-      wordlist.append(yob_full + nick_parts[i])
-      wordlist.append(db + nick_parts[i])
+    # Add the name part followed by the date of birth part, db, yob and yob_full
+    wordlist.append(yob + nick_parts[i])
+    wordlist.append(yob_full + nick_parts[i])
+    wordlist.append(db + nick_parts[i])
 
   # Loop through pet name parts and put the dob at the end
   for l in range(len(petNames_parts)):
@@ -139,13 +126,21 @@ def create_person_wordlist(name, dob):
     wordlist.append(yob_full + hobbies_parts[e])
     wordlist.append(db + hobbies_parts[e])
 
-
-
   # Loop through each item of wordlist and add common symbols to end 
   # Probably needs to be the last thing to run before changin case and swapping numbers
   for k in range(len(wordlist)):
     for h in range(len(symbols)):
       wordlist.append(wordlist[k] + symbols[h])
+
+  # Call caps combinations function to loop through all combinations of capital letters
+  combinations = all_caps_combinations(wordlist)
+  wordlist.append(combinations)
+
+  # Deduplicate (can be improved)
+  deduplicated_list = [] 
+  for word in wordlist: 
+    if word not in deduplicated_list: 
+      deduplicated_list.append(word)
 
 
 # IDEAS:
@@ -155,7 +150,17 @@ def create_person_wordlist(name, dob):
 #
 
   # Return the wordlist
-  return wordlist
+  return deduplicated_list
+
+def all_caps_combinations(wordlist):
+    all_combinations = []
+    for word in wordlist:
+        # Get all combinations of capital letters for the wordlist
+        combinations = [''.join(c) for c in itertools.product(*((char.lower(), char.upper()) for char in word))]
+        all_combinations.extend(combinations)
+        # Remove duplicates by converting to a set and then back to a list 
+       # all_combinations = list(set(all_combinations))
+    return all_combinations
 
 
 # Ask the user what type of target is in sight
@@ -167,9 +172,14 @@ if type == "o":
   print("\n\nEnter the following information to generate your wordlist:\n")
   name = input("Enter the name of the organisation, with any variations seprated by spaces (e.g Doodoo Dyncmics Doodoo Dymamics Ltd): ") # Maybe change this to be comma seperated
   dob = input("Enter any memorable dates seperated by spaces (e.g Date founded dd/mm/yyyy): ")
+  file_name = input("Name your wordlist: ")
   wordlist = create_org_wordlist(name, dob)
-  print("Here is your wordlist:")
-  print(wordlist) # change this to file creation with date
+  # Open the file in write mode and write each item on a new line
+  with open(file_name, 'w') as file:
+    for item in wordlist:
+      file.write(f"{item}\n")
+  print(f"Contents of the list have been written to {file_name}")
+
   exit
 ######################
 # Personal Questions #
@@ -181,9 +191,13 @@ elif type == "p":
   dob = input("Enter the persons date of birth (dd/mm/yyyy): ")
   hobbies = input ("Enter any hobbies/characters/celebs that they like, seperated by spaces: ")
   petNames = input("Enter their pets name seperated by spaces: ")
-  wordlist = create_person_wordlist(name, dob)
-  print("Here is your wordlist:")
-  print(wordlist) # change this to file creation with date
+  file_name = input("Name your wordlist: ")
+  deduplicated_list = create_person_wordlist(name, dob)
+  # Open the file in write mode and write each item on a new line
+  with open(file_name, 'w') as file:
+    for item in deduplicated_list:
+      file.write(f"{item}\n")
+  print(f"Contents of the list have been written to {file_name}")
   exit
 else:
   print("I gave you two options, chose one of them next time")
