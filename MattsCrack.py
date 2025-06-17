8c40fd87274d53b9fd9d684d14008c97ce9dc1d5
NuclearNutshell <matthewk_ames@hotmail.com>
2025-06-17 17:37:37 +0100

    Remove console updates.
    Removes the print functions displaying each generated word. This speeds up the script 10 fold.

--- MattsCrack.py
+++ MattsCrack.py
@@ -11,6 +11,7 @@ letter_to_number = {
     'o': ['0'],
     's': ['5'],
     't': ['7'],
+    'a': ['@'],
 }
 
 # Function to replace letters with similar numbers
@@ -48,9 +49,6 @@ def create_org_wordlist(name, dob):
   # Initialize an empty set to store the words
   wordlist = set()
 
-  # Initialise a list to store commonly used symbols
-  symbols = ["!", "?", "*", "#", "$", "123", "1234", "123!", "1234!"]
-
   # Combine inputs in different ways
   print("\nCombining inputs...")
   for part in name_parts: 
@@ -76,39 +74,49 @@ def create_person_wordlist(name, dob, petNames, nick, hobbies):
   # Initialize an empty set to store the words
   wordlist = set()
 
+  #combine parts together
+  #for part in name_parts:
+  #  wordlist.update([part + nick_parts, part + petNames_parts, part + hobbies_parts])
+ # for part in nick_parts:
+#    wordlist.update([part + hobbies_parts, part + petNames_parts, + part + name_parts])
+
   # Combine inputs in different ways
+ # deliminators = ["-", "."]
   for part in name_parts + nick_parts + petNames_parts + hobbies_parts: 
     wordlist.update([part, part + yob, part + yob_full, part + db]) 
     wordlist.update([yob + part, yob_full + part, db + part])
-  
+
+  # Combine the inputs with deliminators inbetween
+  #  for deliminator in deliminators:
+ #     wordlist.update([part + deliminator + yob, part + deliminator + yob_full, part + deliminator + db])
+#      wordlist.update([yob + deliminator + part, yob_full + deliminator + part, db + deliminator + part])
+
   # Initialise a list to store commonly used symbols
-  symbols = ["!", "?", "*", "#", "$", "123", "1234", "123!", "1234!"]
+  symbols = ["!", "?", "*", "#", "$", "123", "1234", "12345"]
   wordlist.update([word + symbol for word in wordlist for symbol in symbols])
 
   # Call number replacment function
-  final_wordlist = set(wordlist) 
-  for word in wordlist: 
+  final_wordlist = set(wordlist)
+  for word in wordlist:
     final_wordlist.update(replace_with_numbers(word))
-    print_overwrite(word)
 
   # Iterate through each combination of capitalisations
   capital_combinations = [''.join(c) for word in final_wordlist for c in itertools.product(*((char.lower(), char.upper()) for char in word))] 
-  for capital_word in capital_combinations: 
-    print_overwrite(capital_word)
   final_wordlist.update(capital_combinations)
 
   return list(final_wordlist)
 
 # Ask the user questions
+print("\nWelcome to Matt's Crack, a custom wordlist generator, choose the type of target:")
 type = input("\nTarget type:\n\n[o] Organisation  [p] Person\n\n")
 if type == "o":
-  print("\n\nEnter the following information to generate your wordlist:\n")
+  print("\nEnter the following information to generate your wordlist:\n")
   name = input("Enter the name of the organisation, with any variations seprated by spaces (e.g Doodoo Dyncmics Doodoo Dymamics Ltd): ") # Maybe change this to be comma seperated
   dob = input("Enter any memorable dates seperated by spaces (e.g Date founded dd/mm/yyyy): ")
   file_name = input("Name your wordlist: ")
   wordlist = create_org_wordlist(name, dob)
 elif type == "p":
-  print("Enter the following information to generate your wordlist:\n")
+  print("\nEnter the following information to generate your wordlist:\n")
   name = input("Enter the persons name (e.g John Smith): ")
   nick = input("Enter the persons nicknames seperated by spaces: ")
   dob = input("Enter the persons date of birth (dd/mm/yyyy): ")
@@ -124,7 +132,7 @@ else:
 with open(file_name, 'w') as file: 
   for item in wordlist: 
     file.write(f"{item}\n")
-# Count amount of words in file
+# Open file in read mode and count amount of words in file
 with open(file_name, 'r') as file:
   line_count = sum(1 for line in file)
 
